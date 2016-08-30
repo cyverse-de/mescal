fc41675cf45cf3aae777376ff985939773772117
Ian McEwen <mian@cyverse.org>
2016-08-30 14:36:43 -0700

    Version 2.8.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.cyverse/mescal "2.8.0"
+(defproject org.cyverse/mescal "2.8.1-SNAPSHOT"
   :description "A Clojure client library for the Agave API."
   :url "https://github.com/cyverse-de/mescal"
   :license {:name "BSD Standard License"
