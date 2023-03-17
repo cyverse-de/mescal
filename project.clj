10ba081de03992c97747438fd04e601b42705d7b
Paul Sarando <psarando@cyverse.org>
2023-03-16 17:36:38 -0700

    CORE-1889 Bump version to 4.0.0-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.cyverse/mescal "3.1.13-SNAPSHOT"
+(defproject org.cyverse/mescal "4.0.0-SNAPSHOT"
   :description "A Clojure client library for the Agave API."
   :url "https://github.com/cyverse-de/mescal"
   :license {:name "BSD Standard License"
