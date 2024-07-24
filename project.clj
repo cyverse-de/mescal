0acc8491b51b51d7a9240b7e9b8bd07402e2b9c7
Sarah Roberts <sonoran.sarah@gmail.com>
2024-07-23 17:51:28 -0700

    3.1.12 release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.cyverse/mescal "3.1.12-SNAPSHOT"
+(defproject org.cyverse/mescal "3.1.12"
   :description "A Clojure client library for the Agave API."
   :url "https://github.com/cyverse-de/mescal"
   :license {:name "BSD Standard License"
